517b3aaf13853722a99ed958142a40959adf3549
Rafael Leinio <rleinio-contractor@circleup.com>
2022-06-16 11:53:12 -0300

    🎉 project scaffold

---
+++ strider_challenge/__metadata__.py
@@ -0,0 +1,8 @@
+"""Metadata module."""
+
+__title__: str = "strider_challenge"
+__description__: str = ""
+__version__: str = "0.1.0"
+__url__: str = "https://github.com/rafaelleinio/strider-challenge"
+__author__: str = "Rafael Leinio"
+__license__: str = "MIT"
